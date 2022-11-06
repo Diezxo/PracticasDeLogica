19906f771ef96899c552ddff274ada9eb5c41f69
Heilin Diaz <Diezxo01@hotmail.com>
2022-11-06 14:22:00 -0400

    Ejercicio 1 Resuelto.

--- Ejercicios en Csharp/Practicas/Program.cs
+++ Ejercicios en Csharp/Practicas/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+
+//Dado un numero, devolver su tabla de multiplicar completa.
+
+
+/// Main
+/// 
+
+
+Console.WriteLine("Escribe un numero");
+int Ntable = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Hasta que numero deseas la tabla");
+int max= Convert.ToInt32(Console.ReadLine());
+int Result; ;
+
+Console.Clear();
+Console.WriteLine($">>>  Tabla del {Ntable}  <<<");
+for (int i = 1; i < max; i++)
+{
+    Result= Ntable*i;
+    Console.WriteLine($"     >>{Ntable}x{i}={Result}");
+}
